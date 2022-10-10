e136352421bb8a7139d278e2e9cfa32367c3f6e1
showaibbinnasir <showaibnishad@gmail.com>
2022-10-10 09:13:55 -0700

    navigationbar added

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,27 +1,23 @@
 import { Navbar } from 'flowbite-react';
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './Navigation.css'
 
 const Navigation = () => {
     return (
-        <div>
+        <div className='navlink text-center'>
             <Navbar
                 fluid={true}
                 rounded={true}
             >
                 <Navbar.Brand href="https://flowbite.com/">
-                    <img
-                    src="https://flowbite.com/docs/images/logo.svg"
-                    className="mr-3 h-6 sm:h-9"
-                    alt="Flowbite Logo"
-                    />
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                    Flowbite
-                    </span>
+                    <h1 className='text-xl font-semibold'>Web-<span className='text-yellow-300 shadow-md p-1'>School</span></h1>
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse>
-                    <NavLink to='/home'>Home</NavLink>
+                    <NavLink
+                    className={({isActive}) => isActive? 'active' : undefined}
+                     to='/home'>Home</NavLink>
                     <NavLink to='/topic'>Topic</NavLink>
                     <NavLink to='/stats'>Stats</NavLink>
                     <NavLink to='/blog'>Blog</NavLink> 
