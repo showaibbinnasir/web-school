77299dd7b4271fa21bba111ff791b2633da8f0da
showaibbinnasir <showaibnishad@gmail.com>
2022-10-10 08:32:46 -0700

    component folder and router file added

---
+++ src/component/Routes/Routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {path : '/',
+    element : <div>This is Home page</div>
+    }
+])
+
+
+export default router;
