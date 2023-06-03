68af0707308d2ca10dbdad1c9ee53c27d1379ca0
showaibbinnasir <showaibnishad@gmail.com>
2023-06-03 02:21:18 -0500

    updated v4

--- src/component/Option/Option.js
+++ src/component/Option/Option.js
@@ -2,46 +2,38 @@ import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import './Option.css'
 
-const Option = ({option, answere, initState, correct, lock}) => {
+const Option = ({option, answere}) => {
     const notify = () => toast("Correct Answere");
     const wrongnotify = () => toast("Wrong Answere");
-    let total = 0;
+    
     const isClicked = (option) => {
         
         if(answere === option){
             
            
-            correct(true)
+            notify()
            
         }
         
         
         else{
-            correct(false)
-            lock=true;
+            
             wrongnotify()
         }
         
 
         
     }   
-    console.log(initState);
+    
     
     return (
         <div>
-            {
-                lock ? <div className='optionItem'  data-aos="zoom-in-down">
-            
-                <h1>{option}</h1>
-                
-                
-                </div> : <div onClick={()=>isClicked(option)} className='optionItem'  data-aos="zoom-in-down">
+            <div onClick={()=>isClicked(option)} className='optionItem'  data-aos="zoom-in-down">
             
             <h1>{option}</h1>
             
             
             </div>
-            }
             <ToastContainer></ToastContainer>
             
         </div>
--- src/component/Questions/Questions.js
+++ src/component/Questions/Questions.js
@@ -9,37 +9,10 @@ const Questions = ({ question, total }) => {
     const correctAns = () => {
         toast(`${question.correctAnswer}`);
     }
-    let answere = [];
-    const [correctedAns, setCorrectedAns] = useState(null);
-    let totalAnswere = 0;
-    if(correctedAns === true){
-        totalAnswere = 1;
-        answere.push(totalAnswere)
-
-    }
-    if(correctedAns === false){
-        totalAnswere = 0;
-        answere.push(totalAnswere)
-
-    }
-    console.log(totalAnswere);
-    console.log(answere);
-    let lock = false;
+    
 
     return (
-        lock ? <div className='questionContainer text-white bg-black'>
-            <div className='flex justify-between'>
-                <h1 className='text-lg font-semibold '>Question : <span className='text-yellow-400'>{question.question}</span></h1>
-                <EyeIcon onClick={correctAns} className="h-6 w-6 text-white" title='show answere'></EyeIcon>
-            </div>
-            <h1>Options : </h1>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5'>
-                {
-                    question.options.map(option => <Option key={Math.random() * 100} option={option} answere={question.correctAnswer} correct={setCorrectedAns} lock={lock}></Option>)
-                }
-            </div>
-        </div> :
-            <div className='questionContainer text-white'>
+        <div className='questionContainer text-white'>
                 <div className='flex justify-between'>
                     <h1 className='text-lg font-semibold '>Question : <span className='text-yellow-400'>{question.question}</span></h1>
                     <EyeIcon onClick={correctAns} className="h-6 w-6 text-white" title='show answere'></EyeIcon>
@@ -47,10 +20,11 @@ const Questions = ({ question, total }) => {
                 <h1>Options : </h1>
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5'>
                     {
-                        question.options.map(option => <Option key={Math.random() * 100} option={option} answere={question.correctAnswer} initState={correctedAns} correct={setCorrectedAns} lock={lock}></Option>)
+                        question.options.map(option => <Option key={Math.random() * 100} option={option} answere={question.correctAnswer} ></Option>)
                     }
                 </div>
             </div>
+            
     );
 };
 
